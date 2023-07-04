9490606353da90062dc794dc4ef0614c41670c8a
GrantHee <1796954825@qq.com>
2023-07-04 16:27:31 +0800

    Vuetest
    把这些全部放入vue项目，能替换的就替换

---
+++ vuetest/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<!-- HelloWorld.vue -->
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+/* 样式代码 */
+</style>
---
+++ vuetest/src/components/TodoItem.vue
@@ -0,0 +1,21 @@
+<!-- TodoItem.vue -->
+<template>
+    <li>{{ todo.text }}</li>
+  </template>
+  
+  <script>
+  export default {
+    name: 'TodoItem',
+    props: {
+      todo: {
+        type: Object,
+        required: true
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  /* 样式代码 */
+  </style>
+  
