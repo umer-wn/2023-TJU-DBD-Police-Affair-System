7f0931ca99be857ce0987d1c7f1562ee667c34c2
Luo Haoming <2376361708@qq.com>
2023-07-06 10:40:30 +0800

    未知修改-
    7/6/10:30 基础界面

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,28 +1,48 @@
+<!-- 页面头图，请将image-2加到assets文件夹中 -->
 <template>
-  <div>
-    <input type="text" v-model="inputText" placeholder="输入文本">
-    <div class="box">{{ boxContent }}</div>
+  <div class="container">
+    <div class="header">
+      <div class="title">警务处理系统</div>
+    </div>
+
+    <div class="content">
+      <div class="subtitle">警局信息</div>
+    </div>
+    <!-- 此处为返回首页按钮，还没加链接 -->
+    <div class="footer">返回首页</div>
+    <!-- 此处为表示退回上一级的箭头，还没加链接 -->
+    <svg class="arrow" width="20" height="22" viewBox="0 0 20 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <line y1="-2.5" x2="20.4742" y2="-2.5" transform="matrix(-0.814031 0.580822 -0.814031 -0.580822 16.6667 0)" stroke="white" stroke-width="5" />
+      <line y1="-2.5" x2="20.4742" y2="-2.5" transform="matrix(-0.814031 -0.580822 0.814031 -0.580822 20 22)" stroke="white" stroke-width="5" />
+    </svg>
+
+    <img class="logo" src="./assets/image-2.png" />
   </div>
 </template>
+
 
 <script>
 import axios from 'axios';
 
 export default {
+
+
   data() {
     return {
       inputText: '', // 存储输入框的值
       boxContent: '' // 存储box的内容
     };
   },
+
+
   watch: {
     inputText(newValue) {
       // 每当inputText的值发生变化时，执行相应的操作
-      axios.post('http://localhost:7078/api/query', { inputText: newValue })
+      axios.post('http://localhost:7078/api/query', { inputText: newValue})
         .then(response => {
           // 请求成功的处理逻辑
           this.boxContent = response.data;
-          console.log(response.data);
+          //console.log(response.data);
         })
         .catch(error => {
           // 请求失败的处理逻辑
@@ -30,24 +50,79 @@ export default {
         });
     }
   }
+
+
 };
 </script>
 
-<style>
-.box {
-  width: 700px;
-  height: 2000px;
-  border: 1px solid #000;
-  padding: 10px;
+
+<style scoped>
+.container {
+  background: #ffffff;
+  width: calc(100vw - 20px);
+  height: 600px;
+  position: relative;
+  overflow: hidden;
 }
 
-.name-list {
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  gap: 10px;
+.header {
+  background: #0b71bb;
+  width: calc(100vw - 20px);
+  height: 70px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  padding-left: 75px;
 }
 
-.name-item {
-  list-style: none;
+.title {
+  color: #ffffff;
+  text-align: left;
+  font: 400 36px "Inter", sans-serif;
+}
+
+.content {
+  width: calc(100vw - 20px);
+  height: 38px;
+  position: absolute;
+  top: 70px;
+  left: 0;
+  display: flex;
+  align-items: center;
+  padding-left: 75px;
+  background: #b7b3b3;
+  box-shadow: inset 200px 0px 50px 0px rgba(0, 0, 0, 0.25);
+}
+
+.subtitle {
+  color: #ffffff;
+  text-align: left;
+  font: 400 28px "Inter", sans-serif;
+}
+
+.footer {
+  color: #ffffff;
+  text-align: left;
+  font: 400 16px "Inter", sans-serif;
+  position: absolute;
+  top: 42px;
+  right: 20px;
+}
+
+.arrow {
+  position: absolute;
+  top: 77px;
+  left: 16px;
+  overflow: visible;
+}
+
+.logo {
+  width: 70px;
+  height: 70px;
+  position: absolute;
+  top: 0px;
+  left: 0px;
 }
 </style>
