1c44d14d533247d0ab7c60d30ba1605a8ccac9ab
GrantHee <1796954825@qq.com>
2023-07-09 16:11:17 +0800

    Revert "Merge branch 'group2' into main"
    This reverts commit 719911977cd91ab407741ff56ecc4390fe54259e.

--- back/test_connect/Program.cs
+++ back/test_connect/Program.cs
@@ -1,8 +1,6 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 using Oracle.ManagedDataAccess.Client;
-using System.Text;
 
 public class Program
 {
@@ -31,93 +29,23 @@ public class Program
                     // 添加控制器
                     services.AddControllers();
 
-                    // 添加 CORS 配置
+                    // 添加CORS策略
                     services.AddCors(options =>
                     {
-                        options.AddDefaultPolicy(builder =>
+                        options.AddPolicy("AllowCors", builder =>
                         {
-                            builder.WithOrigins("http://localhost:8080") // 允许的前端应用地址
+                            builder.AllowAnyOrigin()
                                 .AllowAnyMethod()
                                 .AllowAnyHeader();
                         });
                     });
-
-                    // 配置JWT
-                    services.AddAuthentication(options =>
-                    {
-                        options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-                    }).AddJwtBearer(options =>
-                    {
-                        options.TokenValidationParameters = new TokenValidationParameters()
-                        {
-                            ValidateIssuer = true,
-                            ValidIssuer = "PoliceApp",
-                            ValidateAudience = true,
-                            ValidAudience = "PoliceAppUser",
-                            ValidateIssuerSigningKey = true,
-                            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
-                                    "123cdefefefeasd14a5445411sds65d4asw65f4e")),
-                            ValidateLifetime = true,
-                            ClockSkew = TimeSpan.FromMinutes(10),
-                            RequireExpirationTime = true,
-                        };
-                    });
-                    services.AddHttpContextAccessor();
-                    services.AddSwaggerGen(options =>
-                    {
-                        options.SwaggerDoc("v1", new OpenApiInfo
-                        {
-                            Version = "v1",
-                            Title = "API标题",
-                            Description = "API描述"
-                        });
-                    });
-                    services.AddSwaggerGen(c =>
-                    {
-                        //添加Jwt验证设置,添加请求头信息
-                        c.AddSecurityRequirement(new OpenApiSecurityRequirement
-                        {
-                            {
-                                new OpenApiSecurityScheme
-                                {
-                                    Reference = new OpenApiReference
-                                    {
-                                        Id = "Bearer",
-                                        Type = ReferenceType.SecurityScheme
-                                    }
-                                },
-                                new List<string>()
-                            }
-                        });
-
-                        //放置接口Auth授权按钮
-                        c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-                        {
-                            Description = "Value Bearer {token}",
-                            Name = "Authorization",//jwt默认的参数名称
-                            In = ParameterLocation.Header,//jwt默认存放Authorization信息的位置(请求头中)
-                            Type = SecuritySchemeType.ApiKey
-                        });
-                    });
-
                 })
                 .Configure(app =>
                 {
                     app.UseRouting();
 
-                    // 添加 CORS 中间件
-                    app.UseCors();
-
-                    app.UseAuthentication();
-                    app.UseAuthorization();
-
-
-
-                    app.UseSwagger();
-                    app.UseSwaggerUI(c =>
-                    {
-                        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Web1");
-                    });
+                    // 启用CORS中间件
+                    app.UseCors("AllowCors");
 
                     app.UseEndpoints(endpoints =>
                     {
