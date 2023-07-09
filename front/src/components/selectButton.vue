768aa35aa6c115a4ba93998d9e288bd5b559c170
zjpyrx <xxhtly@163.com>
2023-07-09 17:22:46 +0800

    前端测试上传

---
+++ front/src/components/selectButton.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+      <button id="bb" class="both">筛选</button>
+    </div>
+</template>
+
+<script>
+</script>
+  
+<style>
+  .both {
+      position: relative;
+      width: 200px;
+      height: 64px;
+      line-height: 64px;
+      box-shadow: inset 0 0 0 3px #000000;
+      margin: 50px auto;
+      text-align: center;
+      color: #000000;
+      font-size: 32px;
+      cursor: pointer;
+      transition: color 1s;
+  }
+  
+  .both::before,
+  .both::after {
+      content: "";
+      position: absolute;
+      width: 0;
+      height: 0;
+      top: 0;
+      left: 0;
+      box-sizing: border-box;
+      // transition: width .5s, height .5s;
+      // transition-delay: .5s, 0s;
+      border: 3px solid transparent;
+  }
+  
+  .both:hover {
+      color: #00e2ff;
+      
+      &::before {
+          transition: width .5s, height .5s, border-bottom-color 0s;
+          transition-delay: .5s, 0s, .5s;
+          width: 200px;
+          height: 64px;
+          border-left: 3px solid #00e2ff;
+          border-bottom: 3px solid #00e2ff;
+      }
+      
+      &::after {
+          transition: width .5s, height .5s, border-right-color .5s;
+          transition-delay: 0s, .5s, .5s;
+          width: 200px;
+          height: 64px;
+          border-top: 3px solid #00e2ff;
+          border-right: 3px solid #00e2ff;
+      }
+  }
+  </style>
--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import searchWagesRecord from '@/components/searchWagesRecord.vue'
+import newRecord from '@/components/newRecord.vue'
+import detailPage from '@/components/detailPage.vue'
+import dataStatistics from '@/components/dataStatistics.vue'
 
 Vue.use(Router)
 
@@ -8,8 +11,23 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'searchWagesRecord',
+      component: searchWagesRecord
+    },
+    {
+      path: '/newRecord',
+      name: 'newRecord',
+      component: newRecord
+    },
+    {
+      path: '/detailPage/:id',
+      name: 'detailPage',
+      component: detailPage
+    },
+    {
+      path: '/dataStatistics',
+      name: 'dataStatistics',
+      component: dataStatistics
     }
   ]
 })
